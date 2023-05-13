857daf5b4bbd5bb8e441842a28970332f912da35
Princess Kena <kenaprincess3@gmail.com>
2023-05-13 15:42:57 -0800

    initial commit

---
+++ js1.js
@@ -0,0 +1,17 @@
+console.log("Hello word");
+
+var name = 'Baaba'
+console.log(name);
+name ='Aunty princes'
+
+
+let age = 90;
+console.log(age);
+
+const drink = true;
+console.log(drink);
+
+
+
+//co nst are not changeable
+// this is an example of comments
